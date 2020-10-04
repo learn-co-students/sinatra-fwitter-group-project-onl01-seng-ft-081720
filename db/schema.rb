255e224a198ad32035eec50c47be2cf397e3488a
Kenneth Colon <kennethmanuel250@gmail.com>
2020-10-04 17:32:41 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_04_032433) do
+ActiveRecord::Schema.define(version: 2020_10_04_165200) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
   end
 
-  create_table "user", force: :cascade do |t|
-    t.string "username"
-    t.text "email"
-    t.string "password_digest"
-  end
-
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "name"
     t.text "email"
     t.string "password_digest"
   end
