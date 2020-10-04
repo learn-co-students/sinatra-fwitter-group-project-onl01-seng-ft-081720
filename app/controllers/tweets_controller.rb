6e67d1af93cb78562a28321d33fcd97b8a445253
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-10-03 22:39:30 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,37 @@
 class TweetsController < ApplicationController
 
+      get "/tweets" do
+        if logged_in?
+          @tweets = Tweet.all
+          erb :'tweets/tweets'
+        else
+          redirect to '/login'
+        end      
+      end
+      
+      get "/tweets/new" do
+        erb :"tweets/new"
+      end
+      
+      post "/tweets" do
+        
+      end
+      
+      get "/tweets/:id" do
+        erb :"tweets/show"
+      end
+      
+      get "/tweets/:id/edit" do
+      
+        erb :"tweets/edit"
+      end
+      
+      post "/tweets/:id" do
+        
+      end
+      
+      delete "/tweets/:id/delete" do
+        
+      end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
 
+      get "/signup" do
+        if logged_in?
+          redirect to "/tweets"
+        else
+          erb :"users/create_user"
+        end
+      end
+      
+      post "/signup" do
+        
+        if params[:username] != "" && params[:email] != "" && params[:password] != "" 
+          user= User.create(params)
+          session[:user_id]=user.id
+          redirect to "/tweets"
+        else 
+          redirect to "/signup"
+        end
+
+      end
+      
+      get "/login" do
+        
+      end
+      
+      post "/login" do
+        
+      end
+      
+      get "/logout" do
+        session.clear
+		    redirect "/"
+      end
 
 end
